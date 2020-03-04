4ce7c26107e8d98621c3ea660e0da94f4c4832bd
José Carlos <jcolscript@gmail.com>
2020-03-04 03:23:27 -0300

    refactorie in timestamp

--- src/database/seeds/20200228033004-admin-user.js
+++ src/database/seeds/20200228033004-admin-user.js
@@ -1,5 +1,4 @@
 const bcrypt = require('bcrypt-nodejs');
-const uuidv4 = require('uuid/v4');
 
 module.exports = {
   up: QueryInterface => {
@@ -7,13 +6,12 @@ module.exports = {
       'users',
       [
         {
-          id: uuidv4(),
           name: 'Distribuidora FastFeet',
           email: 'admin@fastfeet.com',
           cellphone: 11977234567,
           password: bcrypt.hashSync('123456', bcrypt.genSaltSync(8)),
-          createdAt: new Date(),
-          updatedAt: new Date(),
+          created_at: new Date(),
+          updated_at: new Date(),
         },
       ],
       {}
