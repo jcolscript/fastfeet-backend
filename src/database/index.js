a97ae1189f484abe030b5981677f855cfa1f915b
José Carlos <jcolscript@gmail.com>
2020-03-13 03:03:28 -0300

    created model deliverymen

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import Recipients from '../app/models/Recipients';
+import Deliverymen from '../app/models/Deliverymen';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipients];
+const models = [User, Recipients, Deliverymen];
 
 class Database {
   constructor() {
