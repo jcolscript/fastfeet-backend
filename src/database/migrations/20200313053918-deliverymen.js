4d3eead7f3490d4945d444cac58edfbe11813e64
José Carlos <jcolscript@gmail.com>
2020-03-13 02:44:32 -0300

    refactor in deliverymen migration

--- src/database/migrations/20200313053918-deliverymen.js
+++ src/database/migrations/20200313053918-deliverymen.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
+    return queryInterface.createTable('deliverymen', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -27,6 +27,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable('deliverymen');
   },
 };
