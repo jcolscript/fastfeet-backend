0bd8f25fdfe55cd7fdbedc24459afee330ba05f8
José Carlos <jcolscript@gmail.com>
2020-03-18 01:10:04 -0300

    creating upload files endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,23 @@
 import { Router } from 'express';
+import multer from 'multer';
 
-// Middlewares
-import auth from './app/middlewares/auth';
+// Configs
+import multerConfig from './config/multer';
 
 // Controllers
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+
+// Middlewares
+import auth from './app/middlewares/auth';
 
 // Validators
 import Validators from './app/validators';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // Sessions
 routes.post('/sessions', Validators.sessionStore, SessionController.store);
@@ -29,5 +35,8 @@ routes.post(
 );
 routes.put('/deliverymen', auth, DeliverymanController.update);
 routes.delete('/deliverymen/:id', auth, DeliverymanController.delete);
+
+// Files
+routes.post('/files', auth, upload.single('file'), FileController.store);
 
 export default routes;
