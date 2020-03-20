2f67fe852c1df04485f0f83eb302b068ee7d685b
José Carlos <jcolscript@gmail.com>
2020-03-20 01:47:35 -0300

    update reicipient

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -17,6 +17,25 @@ class RecipientController {
       .status(201)
       .json({ id, name, street, number, complement, city, state, postal_code });
   }
+
+  async update(req, res) {
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      postal_code,
+    } = await recipient.update(req.body);
+
+    return res
+      .status(201)
+      .json({ id, name, street, number, complement, city, state, postal_code });
+  }
 }
 
 export default new RecipientController();
