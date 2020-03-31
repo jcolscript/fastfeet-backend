3887b51feb64b17b38a400fabd7d4ac1359bc806
José Carlos <jcolscript@gmail.com>
2020-03-31 00:43:25 -0300

    ajustment in start delivery controller name

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import RecipientController from './app/controllers/RecipientController';
 import FileController from './app/controllers/FileController';
 import DeliverymanController from './app/controllers/DeliverymanController';
 import OrderController from './app/controllers/OrderController';
-import StartDeliveryControlle from './app/controllers/StartDeliveryControlle';
+import StartDeliveryController from './app/controllers/StartDeliveryController';
 
 // Middlewares
 import auth from './app/middlewares/auth';
@@ -44,7 +44,7 @@ routes.get('/orders', auth, OrderController.index);
 routes.post('/orders', auth, OrderController.store);
 routes.put('/orders/:id', auth, OrderController.update);
 routes.delete('/orders/:id', auth, OrderController.delete);
-routes.put('/orders/:orderId/start', StartDeliveryControlle.update);
+routes.put('/orders/:orderId/start', StartDeliveryController.update);
 
 // Files
 routes.post('/files', auth, upload.single('file'), FileController.store);
